691932169a83c10c9da00ac6e55dc35630c469e2
James <jamescscalise@gmail.com>
2020-10-11 13:08:50 -0500

    fixed keys

--- bot.js
+++ bot.js
@@ -2,31 +2,17 @@ const config = require('./config');
 const twit = require('twit');
 const T = new twit(config)
 
-function retweet(){
-    params={
-        q: '#got',
-        result_type: 'recent',
-        count: 100
-    }
+const stream = T.stream('statuses/filter', {track: '#JavaScript'});
 
+// use this to log errors from requests
+function responseCallback (err, data, response) {
+ console.log(err);
+}
 
-T.get('search/tweets', params, (err, data, response) => 
-{
-    let tweets = data.statuses
-    if(!err)
-    {
-        for (let dat of tweets)
-        {
-            let retweetId = dat.id_str;
-            T.post('statuses/retweet/:id', {id: retweetId}, (err, response)=>
-            {
-                if (response){
-                    console.log('Retweeted!!!' + retweetId)
-                }else{
-                    console.log("something went wrong")
-                }
-            })
-        }
-    }
-})}
-setInterval(retweet, 15000)
+// event handler
+stream.on('tweet', tweet => {
+   // retweet
+  T.post('statuses/retweet/:id', {id: tweet.id_str}, responseCallback);
+  // like
+  T.post('favorites/create', {id: tweet.id_str}, responseCallback);
+});
